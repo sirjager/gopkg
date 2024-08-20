337a29a042ab2ee02ce387508465715d4eccdf65
Ankur Kumar <18713058+sirjager@users.noreply.github.com>
2024-08-20 13:30:21 +0530

    improv(utils): use of generics to make usable for all data types

--- utils/array.go
+++ utils/array.go
@@ -1,7 +1,7 @@
 package utils
 
-// The function checks if a given string exists in a slice of strings and returns a boolean value.
-func ValueExist(find string, in []string) bool {
+// Generic function to check if a value exists in a slice
+func ValueExist[T comparable](find T, in []T) bool {
 	for _, v := range in {
 		if v == find {
 			return true
@@ -10,26 +10,32 @@ func ValueExist(find string, in []string) bool {
 	return false
 }
 
-// The ReverseArray function takes a list of strings and returns a new list with the elements in
-// reverse order.
-func ReverseArray(list []string) []string {
+// The ReverseArray function takes a list and
+// returns a new list with the elements in reverse order.
+func ReverseArray[T any](list []T) []T {
 	length := len(list)
-	reversed := make([]string, length)
+	reversed := make([]T, length)
 	for i, j := 0, length-1; i < length; i, j = i+1, j-1 {
 		reversed[i] = list[j]
 	}
 	return reversed
 }
 
-func RemoveDuplicates(strList []string) []string {
-	uniqueMap := make(map[string]bool)
-	uniqueList := []string{}
-	for _, str := range strList {
-		if !uniqueMap[str] {
-			uniqueMap[str] = true
-			uniqueList = append(uniqueList, str)
+func RemoveDuplicates[T comparable](list []T) []T {
+	uniqueMap := make(map[T]bool)
+	uniqueList := []T{}
+	for _, item := range list {
+		if !uniqueMap[item] {
+			uniqueMap[item] = true
+			uniqueList = append(uniqueList, item)
 		}
 	}
-
 	return uniqueList
+}
+
+func GetFirstOrFallback[T any](fallback T, arr ...T) T {
+	if len(arr) != 0 {
+		return arr[0]
+	}
+	return fallback
 }
